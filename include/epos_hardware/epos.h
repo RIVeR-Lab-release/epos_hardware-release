5337c8ab3ac512d437f5868d74cbd753cc9dc94b
Mitchell Wills <mwills@wpi.edu>
2015-06-18 22:16:15 -0400

    Rebase from 'debian/indigo/epos_hardware'

--- include/epos_hardware/epos.h
+++ include/epos_hardware/epos.h
@@ -9,6 +9,18 @@
 
 namespace epos_hardware {
 
+#define STATUSWORD(b, v) ((v >> b) & 1)
+#define READY_TO_SWITCH_ON (0)
+#define SWITCHED_ON (1)
+#define ENABLE (2)
+#define FAULT (3)
+#define VOLTAGE_ENABLED (4)
+#define QUICKSTOP (5)
+#define WARNING (7)
+#define TARGET_REACHED (10)
+#define CURRENT_LIMIT_ACTIVE (11)
+
+
 class Epos {
 public:
   typedef enum {
@@ -45,11 +57,18 @@ private:
   double velocity_;
   double effort_;
   double current_;
+  uint16_t statusword_;
 
   double position_cmd_;
   double velocity_cmd_;
+  int max_profile_velocity_;
+  bool halt_velocity_;
+  double torque_constant_;
+  double nominal_current_;
+  double max_current_;
 
   void buildMotorStatus(diagnostic_updater::DiagnosticStatusWrapper &stat);
+  void buildMotorOutputStatus(diagnostic_updater::DiagnosticStatusWrapper &stat);
 };
 
 }
